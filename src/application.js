8f1cdf265ff4ac3c33aebce4317e9ee6066062f8
ana <adscarlos@gmail.com>
2015-09-30 18:46:16 +0100

    adds functionality to buttons

--- src/application.js
+++ src/application.js
@@ -2,8 +2,33 @@ console.log('Hello!');
 
 var thermostat = new Thermostat();
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
   $('#temperature_display').text(thermostat.temperature);
 
+  $('#increase_button').on('click', function() {
+    thermostat.increaseTemp();
+    $('#temperature_display').text(thermostat.temperature);
+  });
+
+  $('#decrease_button').on('click', function() {
+    thermostat.decreaseTemp();
+    $('#temperature_display').text(thermostat.temperature);
+  });
+
+  $('#reset_button').on('click', function() {
+    thermostat.reset();
+    $('#temperature_display').text(thermostat.temperature);
+  });
+
+  $('#power_saving_mode').on('change', function() {
+    if (this.checked) {
+      thermostat.powerSavingOn();
+    } else {
+      thermostat.powerSavingOff();
+
+    }
+
+    $('#temperature_display').text(thermostat.temperature);
+  });
 });
