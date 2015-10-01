8a169468f2f494f1edd82e13532f212bb4018592
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-01 17:29:59 +0100

    Hyphens amended

--- src/application.js
+++ src/application.js
@@ -1,39 +1,31 @@
 console.log('Hello!');
 
 var thermostat = new Thermostat();
-var captureCity = "London"
 
 var updateTemperature = function() {
   $('#temperature_display').text(thermostat.temperature);
   $('#temperature_display').css('color', thermostat.colour);
 };
 
-// (function ($) {
-
 $(document).ready(function() {
-
-  //$('#weather_city').val('London'),
-  // $('#units_metric').attr('checked', 'checked');
-  // processForm();
     updateTemperature();
 
-
-  $('#increase_button').on('click', function() {
+  $('#increase-button').on('click', function() {
     thermostat.increaseTemp();
     updateTemperature();
   });
 
-  $('#decrease_button').on('click', function() {
+  $('#decrease-button').on('click', function() {
     thermostat.decreaseTemp();
     updateTemperature();
   });
 
-  $('#reset_button').on('click', function() {
+  $('#reset-button').on('click', function() {
     thermostat.reset();
     updateTemperature();
   });
 
-  $('#power_saving_mode').on('change', function() {
+  $('#power-saving-mode').on('change', function() {
     if (this.checked) {
       thermostat.powerSavingOn();
     } else {
@@ -42,9 +34,9 @@ $(document).ready(function() {
     updateTemperature();
   });
 
-  $('#weather_status_form').submit(function(event){
+  $('#weather-status-form').submit(function(event){
     event.preventDefault();
-    captureCity = $('#weather_city').val();
+    captureCity = $('#weather-city').val();
     console.log(captureCity);
     processForm();
     updateTemperature();
@@ -60,15 +52,13 @@ $(document).ready(function() {
       dataType: 'jsonp',
       cache: false,
       data: {
-        q: $('#weather_city').val(),
-        units: $('#weather_status_form input[name="units"]:checked').val()
+        q: $('#weather-city').val(),
       },
       success: function (response) {
-        $('#current_city').text(response.name);
-        $('#weather_description').text(response.weather[0].description);
-        $('#weather_temp').text((response.main.temp -273.15).toFixed(1));
-        $('#weather_wind').text(response.wind.speed);
+        $('#current-city').text(response.name);
+        $('#weather-description').text(response.weather[0].description);
+        $('#weather-temp').text((response.main.temp -273.15).toFixed(1));
+        $('#weather-wind').text(response.wind.speed);
       },
     });
   }
-// })(jQuery);
